12e94993b12cfdd9c6f5a9d32b2b04802260b47c
choudhary-jii <aayush.choudhary067@gmail.com>
2025-05-29 18:41:10 +0530

    Fix wsgi.py activate_this.py path

--- socialmedia/wsgi.py
+++ socialmedia/wsgi.py
@@ -25,10 +25,10 @@ if project_home not in sys.path:
 # Set environment variable for Django settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'socialmedia.settings'
 
-# Activate virtual environment
-activate_this = '/home/socialmediacodenicely/social-media/venv/bin/activate_this.py'
-with open(activate_this) as file_:
-    exec(file_.read(), dict(__file__=activate_this))
+# # Activate virtual environment
+# activate_this = '/home/socialmediacodenicely/social-media/venv/bin/activate_this.py'
+# with open(activate_this) as file_:
+#     exec(file_.read(), dict(__file__=activate_this))
 
 # Import Django's WSGI handler
 from django.core.wsgi import get_wsgi_application
